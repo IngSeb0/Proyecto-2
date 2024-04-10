5a203842dd1a12f9d32380b020ee09e957d7c761
monin <monin@localhost>
2024-04-09 21:06:20 -0500

    avance Entrega 2

---
+++ Galeria/src/modelo/Galeria.java
@@ -0,0 +1,19 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import modelo.inventario.Artista;
+import modelo.inventario.Pieza;
+
+public class Galeria {
+
+	private ArrayList<Artista> artistas = new ArrayList<Artista>();
+	
+	private ArrayList<Pieza> piezas = new ArrayList<Pieza>();
+	
+	private HashMap<String, String> usuarios = new HashMap<String, String>();
+
+	
+}
+
---
+++ Galeria/src/modelo/inventario/Escultura.java
@@ -0,0 +1,68 @@
+package modelo.inventario;
+
+import java.util.ArrayList;
+
+import modelo.perfiles.Propietario;
+
+public class Escultura extends Pieza {
+
+	/*Atributos
+	 * 
+	 */
+
+	private int alto;
+	
+	private int ancho;
+	
+	private int profundidad;
+	
+	private int peso;
+	
+	private ArrayList<String > materialesConstruccion = new ArrayList<String>();
+	
+	private boolean electricidad;
+	
+	/* Constructor
+	 * 
+	 */
+	
+	public Escultura(String titulo, String fechaCreacion, String lugarCreacion, Artista artista, Propietario propietario, int alto,
+			int ancho, int profundidad, int peso, ArrayList<String > materialesConstruccion, boolean electricidad) {
+		super(titulo, fechaCreacion, lugarCreacion, artista, propietario);
+		this.alto = alto;
+		this.ancho = ancho;
+		this.profundidad = profundidad;
+		this.peso = peso;
+		this.materialesConstruccion = materialesConstruccion;
+		this.electricidad = electricidad;
+	}
+
+	/* Getters
+	 * 
+	 */
+	
+	public int getAlto() {
+		return alto;
+	}
+
+	public int getAncho() {
+		return ancho;
+	}
+
+	public int getProfundidad() {
+		return profundidad;
+	}
+
+	public int getPeso() {
+		return peso;
+	}
+
+	public ArrayList<String> getMaterialesConstruccion() {
+		return materialesConstruccion;
+	}
+
+	public boolean isElectricidad() {
+		return electricidad;
+	}
+		
+}
